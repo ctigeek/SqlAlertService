5d3323b31bb79d8a0c7e05510ec98ddfcd22318f
Steven Swenson <ctigeek@gmail.com>
2015-02-27 11:16:08 -0500

    First commit of skeleton.

---
+++ SqlAlertService/SqlAlertWindowsService.Designer.cs
@@ -0,0 +1,21 @@
+﻿namespace SqlAlertService
+{
+    partial class SqlAlertWindowsService
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.ServiceName = "SqlAlertService";
+        }
+    }
+}
---
+++ SqlAlertService/SqlAlertWindowsService.cs
@@ -0,0 +1,24 @@
+﻿using System.ServiceProcess;
+
+namespace SqlAlertService
+{
+    public partial class SqlAlertWindowsService : ServiceBase
+    {
+        private readonly ActionManager actionManager;
+        public SqlAlertWindowsService()
+        {
+            InitializeComponent();
+            actionManager = new ActionManager();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            actionManager.Start();
+        }
+
+        protected override void OnStop()
+        {
+            actionManager.Stop();
+        }
+    }
+}
