99c18e9f9488c7f3e42d1fbdcbf758fe3a30b006
Steven Swenson <ctigeek@gmail.com>
2015-03-01 11:56:53 -0500

    Correct exe name in powershell installer.

--- SqlAlertService/InstallSqlAlertService.ps1
+++ SqlAlertService/InstallSqlAlertService.ps1
@@ -1,4 +1,4 @@
 $fullpath = (Get-Item -Path ".\" -Verbose).FullName
 $cred = Get-Credential -Message "This service requires domain privledges."
-New-Service -Name "SqlAlertService" -DisplayName "SqlAlertService" -Credential $cred -StartupType Automatic -BinaryPathName $fullpath\DataCollector.exe -Description "Runs SQL queries and sends an email if the results don't match the expected value."
+New-Service -Name "SqlAlertService" -DisplayName "SqlAlertService" -Credential $cred -StartupType Automatic -BinaryPathName $fullpath\SqlAlertService.exe -Description "Runs SQL queries and sends an email if the results don't match the expected value."
 
